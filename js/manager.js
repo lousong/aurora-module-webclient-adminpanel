0bd50d1db6a4eb4adbe0e775c3c9d2c36742133f
vasil <vasil.sokolov@gmail.com>
2016-08-09 11:42:54 +0300

    removed MODULES tab in AdminPanel

--- js/manager.js
+++ js/manager.js
@@ -19,9 +19,9 @@ module.exports = function (oAppData, iUserRole, bPublic) {
 				SettingsView.registerTab(function () { return require('modules/%ModuleName%/js/views/CommonSettingsPaneView.js'); },
 					'common',
 					TextUtils.i18n('%MODULENAME%/LABEL_COMMON_SETTINGS_TAB'));
-				SettingsView.registerTab(function () { return null; },
-					'modules',
-					TextUtils.i18n('%MODULENAME%/LABEL_MODULES_SETTINGS_TAB'));
+//				SettingsView.registerTab(function () { return null; },
+//					'modules',
+//					TextUtils.i18n('%MODULENAME%/LABEL_MODULES_SETTINGS_TAB'));
 			},
 			getScreens: function () {
 				var oScreens = {};
