e3d611ef4169901f3d33e4dc31559dcea1e74fb6
vasil <vasil.sokolov@gmail.com>
2018-04-17 12:55:58 +0300

    updated licensing annotations

--- Module.php
+++ Module.php
@@ -3,15 +3,18 @@
  * This code is licensed under AGPLv3 license or AfterLogic Software License
  * if commercial version of the product was purchased.
  * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
- * 
- * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
- * @license https://afterlogic.com/products/common-licensing AfterLogic Software License
- * @copyright Copyright (c) 2018, Afterlogic Corp.
  */
 
 namespace Aurora\Modules\AdminPanelWebclient;
 
 /**
+ * This module adds ability to login to the admin panel as a Super Administrator.
+ *
+ * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
+ * @license https://afterlogic.com/products/common-licensing AfterLogic Software License
+ * @copyright Copyright (c) 2018, Afterlogic Corp.
+ *
+ * @package Modules
  * @internal
  */
 class Module extends \Aurora\System\Module\AbstractWebclientModule
