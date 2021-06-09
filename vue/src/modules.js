73742824ed63c244e1fb84dce9056fbb613c94a5
nadine-shi <nadinshi@gmail.com>
2021-06-09 11:31:10 +0300

    Add GMailConnector module

--- vue/src/modules.js
+++ vue/src/modules.js
@@ -3,6 +3,7 @@ export default {
     return [
       await import('src/manager'),
       await import('src/../../../BrandingWebclient/vue/manager'),
+      await import('src/../../../GMailConnector/vue/manager'),
       await import('src/../../../LicensingWebclient/vue/manager'),
       await import('src/../../../MailWebclient/vue/manager'),
       await import('src/../../../CoreWebclient/vue/manager')
