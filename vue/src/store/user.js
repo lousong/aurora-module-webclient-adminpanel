f813b390cac740083baf4df0abfa73399537efee
nadine-shi <nadinshi@gmail.com>
2021-06-17 16:20:50 +0300

    BUG - cannot login/logout

--- vue/src/store/user.js
+++ vue/src/store/user.js
@@ -1,25 +1,34 @@
+import _ from 'lodash'
 import VueCookie from 'vue-cookie'
 
 import UserModel from 'src/classes/user'
 
 import enums from 'src/enums'
-const UserRoles = enums.getUserRoles()
 
 export default {
   namespaced: true,
+
   state: {
     authToken: VueCookie.get('AuthToken') || '',
-    user: null,
+    userRole: null,
+    userPublicId: null,
   },
+
   mutations: {
     setAuthToken (state, v) {
       state.authToken = v
       VueCookie.set('AuthToken', v)
     },
-    setUserData (state, oUserData) {
-      state.user = new UserModel(null, oUserData)
+
+    setUserData (state, userData) {
+      const user = new UserModel(null, userData, userData)
+      if (!_.isEmpty(user)) {
+        state.userRole = user.role
+        state.userPublicId = user.publicId
+      }
     },
   },
+
   actions: {
     parseAppData ({ commit }, oAppData) {
       commit('setUserData', oAppData.User)
@@ -33,23 +42,29 @@ export default {
       commit('setAuthToken', '')
     },
   },
+
   getters: {
     getAuthToken (state) {
       return state.authToken
     },
+
     isAuthorized (state) {
       return state.authToken !== ''
     },
+
     getDataToSave (state) {
       return {
         authToken: state.authToken,
       }
     },
+
     getUserPublicId (state) {
-      return state.user?.publicId || ''
+      return state.userPublicId
     },
+
     isUserSuperAdmin (state) {
-      return state.user?.role === UserRoles.SuperAdmin
+      const UserRoles = enums.getUserRoles()
+      return state.userRole === UserRoles.SuperAdmin
     },
   },
 }
