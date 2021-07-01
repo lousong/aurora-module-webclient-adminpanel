bee7d12ecb2dbea34737f6391fb224ab31e274a0
nadine-shi <nadinshi@gmail.com>
2021-07-01 14:27:42 +0300

    fixed BUG

--- vue/src/classes/tenant.js
+++ vue/src/classes/tenant.js
@@ -2,7 +2,7 @@ import typesUtils from 'src/utils/types'
 import _ from 'lodash'
 
 class TenantModel {
-  constructor (serverData, completeData = null) {
+  constructor (serverData = {}, completeData = null) {
     this.id = typesUtils.pInt(serverData.Id)
     this.name = typesUtils.pString(serverData.Name)
     this.siteName = typesUtils.pString(serverData.SiteName)
