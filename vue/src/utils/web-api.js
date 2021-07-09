c7f04338d5543644a9ea5fe658f4de5ca65fb728
nadine-shi <nadinshi@gmail.com>
2021-07-09 09:27:31 +0300

    BUG - no indication if login/password is wrong

--- vue/src/utils/web-api.js
+++ vue/src/utils/web-api.js
@@ -59,7 +59,7 @@ export default {
             eventBus.$emit('webApi::Response', { moduleName, methodName, parameters, response: response.data })
             const result = response.data.Result
             if (!result && (response.data.ErrorCode || response.data.ErrorMessage || response.data.SubscriptionsResult)) {
-              if (methodName !== 'Logout' && errors.isAuthError(response.data.ErrorCode)) {
+              if (store.getters['user/isUserSuperAdmin'] && errors.isAuthError(response.data.ErrorCode) && methodName !== 'Logout') {
                 core.logout()
               } else {
                 reject(response.data)
