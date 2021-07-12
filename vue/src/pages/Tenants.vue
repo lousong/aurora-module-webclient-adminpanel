007aa0e93020374aee565922bddf376779027e53
Timofeev Vadim <vtimofeev223@gmail.com>
2021-07-12 16:59:44 +0300

    FIxed - Counter of selected list itmes must be at right

--- vue/src/pages/Tenants.vue
+++ vue/src/pages/Tenants.vue
@@ -4,9 +4,10 @@
     <template v-slot:before>
       <div class="flex column full-height ">
         <q-toolbar class="col-auto q-my-sm">
-          <q-btn flat color="grey-8" size="mg" :label="countLabel" :disable="checkedIds.length === 0"
+          <q-btn flat color="grey-8" size="mg" :disable="checkedIds.length === 0"
                  @click="askDeleteCheckedTenants">
             <Trash></Trash>
+            <span>{{ countLabel }}</span>
             <q-tooltip>
               {{ $t('COREWEBCLIENT.ACTION_DELETE') }}
             </q-tooltip>
